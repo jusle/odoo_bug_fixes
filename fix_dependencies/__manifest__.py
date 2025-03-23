398da9be4b318b1f75f49a89eebc37b3aab5848f
Jussi Lehto <jussi.lehto@cetmix.com>
2025-03-23 02:37:00 +0200

    fix

--- fix_dependencies/__manifest__.py
+++ fix_dependencies/__manifest__.py
@@ -10,4 +10,9 @@
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "installable": True,
+    "external_dependencies": {
+        "python": [
+            "phonenumbers",
+        ],
+    },
 }
