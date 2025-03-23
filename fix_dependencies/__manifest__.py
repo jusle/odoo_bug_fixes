31bd1bd1011900ec1fe712355f9a47be1267ccd0
Jussi Lehto <jussi.lehto@cetmix.com>
2025-03-23 02:11:30 +0200

    fix dependencies

---
+++ fix_dependencies/__manifest__.py
@@ -0,0 +1,13 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Fix dependencies",
+    "summary": """
+        A dummy module that fixes Odoo dependencies bugs.""",
+    "author": "Ab Cetmix Nordic Oy",
+    "website": "https://www.cetmix.fi",
+    "category": "Technical",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "installable": True,
+}
