23748677888f71d6e0fae9f87f60504240319679
Jussi Lehto <jussi.lehto@cetmix.com>
2026-02-21 16:14:47 +0200

    add 'checked' to expections at https://github.com/OCA/account-invoicing/blob/18.0/account_move_tier_validation/models/account_move.py

---
+++ account_move_tier_validation_fix/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Account Move Tier Validation Fix',
+    'version': '18.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Allow to edit the field "checked" when confirming account moves.',
+    'author': 'Ab Cetmix Nordic Oy',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_move_tier_validation',
+    ],
+    'data': [],
+    'installable': True,
+    'auto_install': False,
+}
